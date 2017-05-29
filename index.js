42996043407897b1f68c8bbcf4317209f03d020c
Per Liedman <per.liedman@dotnetmentor.se>
2017-05-29 12:49:15 +0200

    Restructure/refactor and add progress bar

--- index.js
+++ index.js
@@ -3,110 +3,171 @@ var fs = require('fs'),
     osmium = require('osmium'),
     TileSet = require('node-hgt').TileSet,
     haversine = require('./haversine'),
-    argv = require('minimist')(process.argv.slice(2)),
-    tileSet = new TileSet(argv.cache_dir || './data/'),
-    file = new osmium.File(argv._[0]),
-    reader = new osmium.Reader(file, { node: true, way: true }),
-    locationHandler = new osmium.LocationHandler(),
-    handler = new osmium.Handler(),
-    wayinfoMap = {},
-    completed = 0,
-    tasks;
+    status = require('node-status'),
+    console = status.console();
 
-handler.on('way', function(way) {
-    if (way.tags('highway')) {
+var argv = require('minimist')(process.argv.slice(2)),
+    tileSet = new TileSet(argv.cache_dir || './data/'),
+    wayinfoMap = {};
+
+if (argv._.length < 2) {
+    console.log('Missing arguments.');
+    console.log('Expected arguments:');
+    console.log(process.argv[0], process.argv[1], '[--cache_dir=CACHE_DIR] [OSM FILE] [DEST FILE]');
+    process.exit(1);
+}
+
+var fillNodeElevations = function (coords, cb) {
+    var wait = coords.length;
+
+    coords.forEach(function(c) {
+        tileSet.getElevation(c, function(err, elevation) {
+            if (!err) {
+                c.elevation = elevation;
+                wait--;
+            } else {
+                cb(err);
+                return;
+            }
+
+            if (wait === 0) {
+                cb(null, coords);
+            }
+        });
+    });
+};
+
+var createWayInfo = function (coords, cb) {
+    var wayInfo = coords.reduce(function(a, c) {
+        var data = a.data,
+            d;
+
+        if (a.lastCoord) {
+            d = haversine(a.lastCoord, c);
+            if (c.elevation > a.lastElevation) {
+                data.climb += c.elevation - a.lastElevation;
+                data.climbDistance += d;
+            } else {
+                data.descent += a.lastElevation - c.elevation;
+                data.descentDistance += d;
+            }
+
+            data.distance += d;
+        }
+
+        a.lastElevation = c.elevation;
+        a.lastCoord = c;
+
+        return a;
+    }, {
+        data: {
+            distance: 0,
+            climbDistance: 0,
+            descentDistance: 0,
+            climb: 0,
+            descent: 0
+        }
+    }).data;
+    cb(null, wayInfo);
+};
+
+var filter = function (way) {
+    return !!way.tags('highway');
+};
+
+var countWays = function (cb) {
+    var file = new osmium.File(argv._[0]),
+        reader = new osmium.Reader(file, { way: true }),
+        handler = new osmium.Handler(),
+        count = 0;
+
+    handler.on('way', function (way) {
+        if (filter(way)) {
+            count++;
+            countWaysJob.inc();
+        }
+    });
+
+    function next () {
+        var buffer = reader.read();
+        if (buffer) {
+            osmium.apply(buffer, handler);
+            setImmediate(next);        
+        } else {
+            cb(count);
+        }
+    }
+
+    next();
+}
+
+var wayHandler = function(tasks, way) {
+    if (filter(way)) {
         try {
             var wayId = way.id,
                 coords = way.node_coordinates()
                         .map(function(c) { return { lat: c.lat, lng: c.lon }; });
         } catch (e) {
-            console.warn(e);
+            console.warn('Error for way', way.id, ': ' + e.message);
             return;
         }
         tasks.push(function(cb) {
-            var wait,
-                createWayInfo = function() {
-                    var wayInfo = coords.reduce(function(a, c) {
-                        var data = a.data,
-                            d;
-
-                        if (a.lastCoord) {
-                            d = haversine(a.lastCoord, c);
-                            if (c.elevation > a.lastElevation) {
-                                data.climb += c.elevation - a.lastElevation;
-                                data.climbDistance += d;
-                            } else {
-                                data.descent += a.lastElevation - c.elevation;
-                                data.descentDistance += d;
-                            }
-
-                            data.distance += d;
-                        }
-
-                        a.lastElevation = c.elevation;
-                        a.lastCoord = c;
-
-                        return a;
-                    }, {
-                        data: {
-                            distance: 0,
-                            climbDistance: 0,
-                            descentDistance: 0,
-                            climb: 0,
-                            descent: 0
-                        }
-                    }).data;
-
+            processWaysJob.inc();
+            async.waterfall([
+                fillNodeElevations.bind(this, coords),
+                createWayInfo], function(err, wayInfo) {
+                    if (err) {
+                        return cb(err);
+                    }
                     wayinfoMap[wayId] = wayInfo;
-                    setImmediate(function() {
-                        cb(undefined, wayInfo);
-                    });
-                };
-
-            wait = coords.length;
-
-            coords.forEach(function(c) {
-                tileSet.getElevation(c, function(err, elevation) {
-                    if (!err) {
-                        c.elevation = elevation;
-                        wait--;
-                    } else {
-                        console.log(err);
-                        cb(err);
-                        return;
-                    }
-
-                    if (wait === 0) {
-                        createWayInfo();
-                    }
+                    cb(undefined, wayInfo);
                 });
-            });
         });
     }
+};
+
+var countWaysJob = status.addItem('count'),
+    processWaysJob;
+
+status.start({
+    pattern: 'Ways: {count.default.green} | ' +
+        'Processed: {process.bar.cyan} {process.percentage.green} | ' +
+        '{uptime.yellow} {spinner.cyan}',
+    precision: 0
 });
 
-function next() {
-    var buffer = reader.read();
-    tasks = [];
+countWays(function (numberWays) {
+    processWaysJob = status.addItem('process', {
+        max: numberWays
+    });
 
-    if (buffer) {
-        osmium.apply(buffer, locationHandler, handler);
-        async.series(tasks, function(err) {
-            if (!err) {
-                completed += tasks.length;
-                if (tasks.length) {
-                    console.log(completed);
+    var file = new osmium.File(argv._[0]),
+        reader = new osmium.Reader(file, { node: true, way: true }),
+        locationHandler = new osmium.LocationHandler(),
+        handler = new osmium.Handler(),
+        tasks = [];
+
+    handler.on('way', wayHandler.bind(this, tasks));
+
+    function next() {
+        tasks.length = 0;
+        var buffer = reader.read();
+
+        if (buffer) {
+            osmium.apply(buffer, locationHandler, handler);
+            async.parallelLimit(tasks, 4, function(err) {
+                if (!err) {
+                    setImmediate(function () { next(); });
+                } else {
+                    console.error(err);
+                    return process.exit(1);
                 }
-                setImmediate(function() {
-                    next();
-                });
-            } else {
-                console.log(err);
-            }
-        });
-    } else {
-        fs.writeFileSync(argv._[1], JSON.stringify(wayinfoMap));
+            });
+        } else {
+            fs.writeFileSync(argv._[1], JSON.stringify(wayinfoMap));
+            process.exit(0);
+        }
     }
-}
 
-next();
+    next();
+});
